51206cd526e8c5d22508dddb77e54b4cf51689e2
Winterflame <95693999+W-nterflame@users.noreply.github.com>
2024-06-27 20:58:00 +0800

    Update OrderHistory.h

--- OrderHistory.h
+++ OrderHistory.h
@@ -27,21 +27,20 @@ public:
         std::ofstream file(filename, std::ios::app); // Append mode
         if (file.is_open()) {
             file << "Order Details\n";
-            file << "Delivery Address: " << order.getDelivery().getAddress() << "\n";
             file << "Delivery Option: " << order.getDelivery().getDeliveryOption() << "\n";
-            file << "Delivery Price: " << order.getDelivery().getDeliveryPrice() << "\n";
+            file << "Delivery Price: $" << order.getDelivery().getDeliveryPrice() << "\n";
             file << "Items Ordered:\n";
             for (size_t i = 0; i < order.getItems().size(); ++i) {
                 file << order.getItems()[i].getName() << ","
                     << order.getQuantities()[i] << ",";
                 if (!order.getItems()[i].getSpecialInstructions().empty()) {
-                    file << order.getItems()[i].getSpecialInstructions();
+                    file << "Special Instructions: " << order.getItems()[i].getSpecialInstructions();
                 }
                 file << "\n";
             }
             file << "Food Utensils: " << (order.getHasUtensils() ? "Yes - $0.5\n" : "No\n");
             file << "Tip: $" << order.getTipAmount() << "\n";
-            file << "Total Amount: " << order.calculateTotal() << "\n";
+            file << "Total Amount: $" << order.calculateTotal() << "\n";
             file << "------------------------------------\n";
             file.close();
         }
@@ -50,9 +49,9 @@ public:
         }
     }
 
-    const std::vector<Order>& getOrderHistory() const {
+    const std::vector<Order>& getOrders() const {
         return orders;
     }
 };
 
-#endif ORDER_HISTORY_H
+#endif // ORDER_HISTORY_H
